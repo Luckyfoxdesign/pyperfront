84f34a105cd229e093388b3bb65d2fe23b4a669d
Maksim Sovenkov <m.sovenkov@semrush.com>
2021-08-17 23:07:04 +0300

    init

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,11 @@
+<script>
+	async function tr() {
+		await fetch("https://bs.devcodebox.com/").then((r) => console.log(r))
+	}
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p>test</p>
 <p>test2</p>
+<button on:click={tr}>test request</button>
